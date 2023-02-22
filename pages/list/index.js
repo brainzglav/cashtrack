04c2c581a6434c62ce08dc2cf7469d6a4549cffc
marzglav <feg.leg.crypto@gmail.com>
2023-02-22 17:47:31 +0100

    More questions

--- pages/list/index.js
+++ pages/list/index.js
@@ -33,7 +33,25 @@ function displayList(items) {
 }
 
 /* 
- Napisi funkciju fetchList koja se koristi za dohvacanje liste studenata sa endpointa /records
+1. Napisi funkciju fetchList koja se koristi za dohvacanje liste studenata sa endpointa /records
  i potom displaya tu funkciju koristeci displayList funkciju. Mozes koristiti axios za to.
- Pozivi tu funkciju pri inicijalizaciji stranice.
+ Kreiraj varijablu u koju ces spremati tu listu u funkciji fetchList, te pozovi tu funkciju
+ pri inicijalizaciji.
+*/
+
+/* 
+ 2. Modificiraj postojecu createListItem funkciju da umjesto UTILITIES, GROCERIES, REPAIRS ,MISCELLANEOUS
+  ispisuje Rezije, Namirnice, Servis, Ostalo. 
+*/
+
+/* 
+ 3. Dodaj funkciju na botun koja ce pri prvom kliku na botun sortirati listu od najveceg prema najmanjem
+  i promjeniti naziv botuna u Descending. Drugim klikom ce sortirati od najmanjem prema najvecem, a 
+  naziv botuna ce biti Ascending. Ponovnim klikom botun ce mjenjati svoje stanje iz Ascendinga u Descending
+  i obrnuto.
+*/
+
+/* 
+ 4. Dodaj funkciju na input koja ce pri change eventu filtrirati listu ovisno o unesenom tekstu. Dakle
+  za unos rez, mora mi izlistati sve elemente s imenom rezije.
 */
