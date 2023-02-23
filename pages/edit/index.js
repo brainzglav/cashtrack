5102a966256a2b5439d8ce0cd1fcc1e47fab70d7
marzglav <feg.leg.crypto@gmail.com>
2023-02-23 17:36:05 +0100

    Get values funkcija dodana

--- pages/edit/index.js
+++ pages/edit/index.js
@@ -1,3 +1,9 @@
+function getValues(form) {
+  const formData = new FormData(form);
+
+  return Object.fromEntries(formData);
+}
+
 /* 
   7. Dodaj submit event listener na formu i odgovarajuci callback. Submit event mora uzeti vrijednosti
      prethodno dodanog selecta i inputa, te provjeriti jesu li unesene. Ukoliko barem jedna vrijednost
